265e366decc27b387d3a65531eb5a43648b4468b
llxff <ll.wg.bin@gmail.com>
2013-03-04 00:23:31 +0400

    story

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,3 @@
+class StoriesController < InheritedResources::Base
+
+end
--- app/models/story.rb
+++ app/models/story.rb
@@ -4,5 +4,10 @@ class Story < ActiveRecord::Base
   as_enum :risk, { low_risk: 0, medium_risk: 1, hight_risk: 3 }
   as_enum :status, { new_status: 0, in_progress: 1, finished: 2, delivered: 3, rejected: 4, accepted: 5 }
 
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :risk_cd, :story_category_id, :status_cd
+
+  validates_presence_of :name, :description, :risk_cd
+
+  validates :story_category_id, numericality: { greater_than:  0 }
+
 end
